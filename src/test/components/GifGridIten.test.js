5dab5f9cdb1288199d25f51d0a254d7f206869fb
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-13 12:35:59 -0300

    creacion de proptypes en <GifGridItem /> title y url isRequired

--- src/test/components/GifGridIten.test.js
+++ src/test/components/GifGridIten.test.js
@@ -4,7 +4,11 @@ import { GifGridItem } from '../../components/GifGridItem'
 
 describe('pruebas en <GifGridItem />', () => {
     test('debe mostrar el commponente correctamente', () => {
-        const wraper = shallow(<GifGridItem />)
+
+        const title = 'soy un titulo'
+        const url ='htpps://localhost/test'
+
+        const wraper = shallow(<GifGridItem title={title} url={url} />)
 
         expect(wraper).toMatchSnapshot()
 
