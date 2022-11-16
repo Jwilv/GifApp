8554f233d83df84d018f1723edbef3980efe1e58
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-16 11:49:10 -0300

    se agrego test de renderizado de <GifGrid /> con toMatchSnapshot

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -1,3 +1,12 @@
-describe('pruebas en GifGrid', () => {
+import React from 'react'
+import { shallow } from 'enzyme'
+import { GifGrid } from '../../components/GifGrid';
+
+describe('pruebas en <GifGrid />', () => {
     
+    test('debe de mostrarse correctamente', () => {
+        const category = 'batman'
+        const wraper =shallow(<GifGrid category={category}/>)
+        expect(wraper).toMatchSnapshot()
+    });
 });
