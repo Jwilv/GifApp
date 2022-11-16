5116120e63953b4348844dfab160157a4d83f24e
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-16 16:13:48 -0300

    test de renderizados y uso de mock para el custom hook

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs')
 
 describe('pruebas en <GifGrid />', () => {
+    const category = 'batman'
     
     test('debe de mostrarse correctamente', () => {
-        const category = 'batman'
-        const wraper =shallow(<GifGrid category={category}/>)
-        expect(wraper).toMatchSnapshot()
+        useFetchGifs.mockReturnValue({
+            data: [],
+        loading: true,
+        });
+        
+        const wrapper =shallow(<GifGrid category={category}/>)
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de mostrar items cuando se cargan imagenes useFetchGifs', () => {
+        
+        const gifs =[{
+            id:'abc',
+            url:'https://localhosto/cualquier/cosa',
+            title:'cualquier titulo'
+        }]
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+        loading: false,
+        });
+        
+        const wrapper =shallow(<GifGrid category={category}/>);
+        expect(wrapper).toMatchSnapshot();
     });
 });
