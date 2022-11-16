c5c31728dc71409a23bac0dad92298ad368386b7
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-11-16 18:53:37 -0300

    test de integracion de componentes en <GifExpertApp /> pasados

--- src/test/components/GifExpertApp.test.js
+++ src/test/components/GifExpertApp.test.js
@@ -3,10 +3,22 @@ import { shallow } from 'enzyme'
 import { GifExperApp } from '../../GifExperApp';
 
 describe('pruebas en <GifExpertApp />', () => {
-    const wrapper = shallow(<GifExperApp />)
+
+    let wrapper = shallow(<GifExperApp />);
+
+    beforeEach( () => {
+        wrapper = shallow(<GifExperApp />);
+    })
+
     test('debe renderizarse correctamente', () => {
-        
     expect(wrapper).toMatchSnapshot();
         
+    });
+
+    test('deben mostrarse correctamente las categorias', () => {
+        const categories = ['batman','hunter x hunter']
+        wrapper = shallow(<GifExperApp defaultCategorie={ categories }/>);
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length)
     });
 });
